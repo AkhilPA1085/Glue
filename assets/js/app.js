89ec90db395bdc143ce3850ea24184af4f8958e5
Akhil P A <akhilpa1085@gamil.com>
2023-04-03 15:09:44 +0530

    media card edited

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,90 +1,83 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
   var scroll = $(window).scrollTop();
-  if(scroll > 100){
-      $('header').addClass('header-scrolled');
-  }else{
-      $('header').removeClass('header-scrolled');
+  if (scroll > 100) {
+    $("header").addClass("header-scrolled");
+  } else {
+    $("header").removeClass("header-scrolled");
   }
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
   var winScroll = $(this).scrollTop();
-  $('.bg3').css({
-    'transform':'translate(0,' + winScroll/8 +'%)'
+  $(".bg3").css({
+    transform: "translate(0," + winScroll / 8 + "%)",
   });
 
-  $('.bg4').css({
-    'transform':'translate(0,' + winScroll/8 +'%)'
+  $(".bg4").css({
+    transform: "translate(0," + winScroll / 8 + "%)",
   });
 
-  $('.bg5').css({
-    'transform':'translate(0,' + winScroll/12 +'%)'
+  $(".bg5").css({
+    transform: "translate(0," + winScroll / 12 + "%)",
   });
 
-  $('.bg6').css({
-    'transform':'translate(0,' + winScroll/6 +'%)'
+  $(".bg6").css({
+    transform: "translate(0," + winScroll / 6 + "%)",
   });
-  $('.bg8').css({
-    'transform':'translate(0,' + winScroll/13 +'%)',
+  $(".bg8").css({
+    transform: "translate(0," + winScroll / 13 + "%)",
   });
- 
-})
-
-
-
+});
 
 // pure counter
 new PureCounter();
 
 var swiper = new Swiper(".leadershipTeamSwiper", {
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 2.2,
-          spaceBetween: 10,
-        },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
+    1024: {
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+  },
 });
 
-
 var swiper = new Swiper(".mediaCenterSwiper", {
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-      },
-  });
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
 
-$(document).ready(function(){
-  if($(window).width() < 992){
-    $('.mediaCenterSwiper .swiper-wrapper.container').removeClass('container');
+$(document).ready(function () {
+  if ($(window).width() < 992) {
+    $(".mediaCenterSwiper .swiper-wrapper.container").removeClass("container");
   }
-})
-
-
+});
